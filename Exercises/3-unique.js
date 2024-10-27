153f8fbaabfd897c99bbc0b7eb33f618197b3d14
Fererra <126345645+Fererra@users.noreply.github.com>
2024-10-27 08:02:16 +0200

    completed task

--- Exercises/3-unique.js
+++ Exercises/3-unique.js
@@ -3,6 +3,14 @@
 // Create and return a new array without duplicate elements
 // Don't modify initial array
 
-const unique = (array) => [];
+const unique = (array) => {
+  const result = [];
+  for (const elem of array) {
+    if (!result.includes(elem)) {
+      result.push(elem);
+    }
+  }
+  return result;
+};
 
 module.exports = { unique };
--- Exercises/4-difference.js
+++ Exercises/4-difference.js
@@ -3,6 +3,14 @@
 // Find difference of two arrays
 // elements from array1 that are not includes in array2
 
-const difference = (array1, array2) => [];
+const difference = (array1, array2) => {
+  const result = [];
+  for (const elem of array1) {
+    if (!array2.includes(elem)) {
+      result.push(elem);
+    }
+  }
+  return result;
+};
 
 module.exports = { difference };
